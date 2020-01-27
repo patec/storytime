acc84da06ecd8b6c5d3c45479c1489432432c365
Pate Cobb <patecobb@gmail.com>
2020-01-26 18:28:12 -0800

    install script

---
+++ ops/install.sh
@@ -0,0 +1,5 @@
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+sudo apt update -y
+sudo apt install --no-install-recommends yarn -y
